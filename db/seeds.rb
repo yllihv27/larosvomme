855858754834ccf1f08173cfd8891fe5d95847d2
jonaspreisler <jonas.preisler@g>
2018-12-09 21:19:14 +0800

    Added days to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ course_categories = CourseCategory.create!([{name: 'Baby'}, {name: 'Småbarn'}, 
 course_niveaus = CourseNiveau.create!([{name: 'Nybegynner'}, {name: 'Øvet'}, {name: 'Videregående'}])
 course_places = CoursePlace.create!([{name: 'Asker Vettre Thon Hotell'}, {name: 'Hokksund Kurbad'}, {name: 'Landøya Barnehage'}, {name: 'Oslo-Sofienbergsenteret'}, {name: 'Risenga Storbassenget(25m)'}, {name: 'Risenga Terapibassenget'}])
 courses = Course.create!([{coach_id: 1, price: '1500', course_niveau_id: 1, course_category_id: 1, course_place_id: 6, time_from: '15:30:00', time_to: '16:00:00'},
-																			{coach_id: 1, price: '1500', course_niveau_id: 2, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00'},
-																			{coach_id: 1, price: '1500', course_niveau_id: 3, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00'},
-																			{coach_id: 1, price: '1500', course_niveau_id: 1, course_category_id: 1, course_place_id: 6, time_from: '15:30:00', time_to: '16:00:00'},
-																			{coach_id: 2, price: '1500', course_niveau_id: 2, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00'},
-																			{coach_id: 2, price: '1500', course_niveau_id: 3, course_category_id: 1, age_from: 1, age_to: 2, course_place_id: 6, time_from: '16:30:00', time_to: '17:00:00'},
-																			{coach_id: 2, price: '1500', course_category_id: 2, age_from: 3, age_to: 4, course_place_id: 6, time_from: '17:00:00', time_to: '17:30:00'},
-																			{coach_id: 2, price: '1500', course_category_id: 2, age_from: 2, age_to: 3, course_place_id: 6, time_from: '16:30:00', time_to: '17:00:00'},
-																			{coach_id: 2, price: '1500', course_category_id: 2, age_from: 3, age_to: 4, course_place_id: 6, time_from: '17:00:00', time_to: '17:30:00'}
+																			{coach_id: 1, price: '1500', course_niveau_id: 2, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00', day: '2018-12-20'},
+																			{coach_id: 1, price: '1500', course_niveau_id: 3, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00', day: '2018-12-22'},
+																			{coach_id: 1, price: '1500', course_niveau_id: 1, course_category_id: 1, course_place_id: 6, time_from: '15:30:00', time_to: '16:00:00', day: '2018-12-21'},
+																			{coach_id: 2, price: '1500', course_niveau_id: 2, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00', day: '2018-12-24'},
+																			{coach_id: 2, price: '1500', course_niveau_id: 3, course_category_id: 1, age_from: 1, age_to: 2, course_place_id: 6, time_from: '16:30:00', time_to: '17:00:00', day: '2018-12-20'},
+																			{coach_id: 2, price: '1500', course_category_id: 2, age_from: 3, age_to: 4, course_place_id: 6, time_from: '17:00:00', time_to: '17:30:00', day: '2018-12-24'},
+																			{coach_id: 2, price: '1500', course_category_id: 2, age_from: 2, age_to: 3, course_place_id: 6, time_from: '16:30:00', time_to: '17:00:00', day: '2018-12-20'},
+																			{coach_id: 2, price: '1500', course_category_id: 2, age_from: 3, age_to: 4, course_place_id: 6, time_from: '17:00:00', time_to: '17:30:00', day: '2018-12-20'}
 																			])
