f0ddfb4e2550c8c215c81c2e1d4fcb4e051eb7a5
jonaspreisler <jonas.preisler@g>
2018-12-09 21:25:50 +0800

    Added day to the last course

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ coaches = Coach.create!([{name: 'Jonas', email: 'jonas.preisler@gmail.com', pass
 course_categories = CourseCategory.create!([{name: 'Baby'}, {name: 'Småbarn'}, {name: 'Barn'}, {name: 'Barnehage Svømning'}, {name: 'Vanntrim'}, {name: 'Triathlonkids'}])
 course_niveaus = CourseNiveau.create!([{name: 'Nybegynner'}, {name: 'Øvet'}, {name: 'Videregående'}])
 course_places = CoursePlace.create!([{name: 'Asker Vettre Thon Hotell'}, {name: 'Hokksund Kurbad'}, {name: 'Landøya Barnehage'}, {name: 'Oslo-Sofienbergsenteret'}, {name: 'Risenga Storbassenget(25m)'}, {name: 'Risenga Terapibassenget'}])
-courses = Course.create!([{coach_id: 1, price: '1500', course_niveau_id: 1, course_category_id: 1, course_place_id: 6, time_from: '15:30:00', time_to: '16:00:00'},
+courses = Course.create!([{coach_id: 1, price: '1500', course_niveau_id: 1, course_category_id: 1, course_place_id: 6, time_from: '15:30:00', time_to: '16:00:00', day: '2018-12-20'},
 																			{coach_id: 1, price: '1500', course_niveau_id: 2, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00', day: '2018-12-20'},
 																			{coach_id: 1, price: '1500', course_niveau_id: 3, course_category_id: 1, course_place_id: 6, time_from: '16:00:00', time_to: '16:30:00', day: '2018-12-22'},
 																			{coach_id: 1, price: '1500', course_niveau_id: 1, course_category_id: 1, course_place_id: 6, time_from: '15:30:00', time_to: '16:00:00', day: '2018-12-21'},
